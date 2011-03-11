0c47a89243b0aa7508db79d8ff43259bf1b83604
Francesco Lazzarino <flazzarino@gmail.com>
2011-03-11 01:27:00 -0500

    classes referenced in purchase_tickets_spec exist

--- spec/requests/purchase_tickets_spec.rb
+++ spec/requests/purchase_tickets_spec.rb
@@ -1,39 +1,47 @@
 describe 'Purchasing Tickets' do
 
+  let :showtime do
+    movie = Movie.create :title => 'Napoleon Dynamite'
+    movie.save
+
+    theater = Theater.create :name => 'Gator Cinemas'
+    theater.save
+
+    s = theater.showtimes.create(:movie => movie)
+    s.save
+    s
+  end
+
   it 'should allow a customer to purchase a ticket for a chosen movie and showtime' do
-    some_showtime = Showtime.make
-
     visit '/'
-    find("a[href='#{showtime_link(some_showtime)}']").click_link
+    find("a[href='#{showtime_path(showtime)}']").click_link
 
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
-    page.should have_content(some_showtime.to_s)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
+    page.should have_content(showtime.to_s)
 
     click_button 'Purchase Tickets'
 
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
     page.should have_content("regular tickets: 1")
     page.should have_content("$10.00")
     page.should have_content("This is your proof, please print")
   end
 
   it 'should allow the number of tickets to be chosen' do
-    some_showtime = Showtime.make
-
     visit '/'
-    find("a[href='#{showtime_link(some_showtime)}']").click_link
+    find("a[href='#{showtime_path(showtime)}']").click_link
 
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
-    page.should have_content(some_showtime.to_s)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
+    page.should have_content(showtime.to_s)
     select '3', :from => 'Tickets'
 
     click_button 'Purchase Tickets'
 
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
     page.should have_content("regular tickets: 2")
     page.should have_content("$20.00")
     page.should have_content("This is your proof, please print")
@@ -42,14 +50,15 @@ describe 'Purchasing Tickets' do
   it 'should inform the user when the number of tickets exceeds the number of avaiable seats' do
     left_over = 5
     want_to_by = left_over + 1
-    some_showtime = Showtime.make :available_tickets => left_over
+    showtime.available_tickets = left_over
+    showtime.save
 
     visit '/'
-    find("a[href='#{showtime_link(some_showtime)}']").click_link
+    find("a[href='#{showtime_path(showtime)}']").click_link
 
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
-    page.should have_content(some_showtime.to_s)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
+    page.should have_content(showtime.to_s)
     select want_to_by.to_s, :from => 'Tickets'
 
     click_button 'Purchase Tickets'
@@ -60,28 +69,29 @@ describe 'Purchasing Tickets' do
 
     select left_over.to_s, :from => 'Tickets'
     click_button 'Purchase Tickets'
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
     page.should have_content("regular tickets: #{left_over}")
     page.should have_content("$25.00")
     page.should have_content("This is your proof, please print")
   end
 
   it 'should sell cheap seats at a 50% discount after all regular seats are sold' do
-    some_showtime = Showtime.make :available_tickets => CHEAP_TIX_AVAILABLE
+    showtime.available_tickets = CHEAP_TIX_AVAILABLE
+    showtime.save
 
     visit '/'
-    find("a[href='#{showtime_link(some_showtime)}']").click_link
+    find("a[href='#{showtime_path(showtime)}']").click_link
 
     page.should have_content("Cheap seats are available, 50% off regular price!")
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
-    page.should have_content(some_showtime.to_s)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
+    page.should have_content(showtime.to_s)
 
     click_button 'Purchase Tickets'
 
-    page.should have_content(some_showtime.theater.name)
-    page.should have_content(some_showtime.movie.title)
+    page.should have_content(showtime.theater.name)
+    page.should have_content(showtime.movie.title)
     page.should have_content("cheap tickets: 1")
     page.should have_content("$5.00")
     page.should have_content("This is your proof, please print")
