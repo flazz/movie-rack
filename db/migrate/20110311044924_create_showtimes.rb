46142bff7099472293b6d8a4108dc7119ec7db61
Francesco Lazzarino <flazzarino@gmail.com>
2011-03-11 18:39:43 -0500

    playing at is back to timestamp

--- db/migrate/20110311044924_create_showtimes.rb
+++ db/migrate/20110311044924_create_showtimes.rb
@@ -2,7 +2,7 @@ class CreateShowtimes < ActiveRecord::Migration
   def self.up
     create_table :showtimes do |t|
       t.integer :available_tickets
-      t.time :playing_at
+      t.timestamp :playing_at
       t.references :movie
       t.references :theater
       t.timestamps
